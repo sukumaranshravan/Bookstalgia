0e63e55d49dd91d4bb1a0f8614f76d73005512a2
sukumaranshravan <shravansukumaran@yahoo.in>
2024-04-17 10:27:19 +0530

    additional route for image by passing on id create

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",async (req,res)=>{
-    const result = await db.query("select * from booklist")
-    console.log(result.rows)
+    const result = await db.query("select * from booklist order by id desc")
+    // console.log(result.rows)
     const books = result.rows
     let value = books[0].isbn
     // const API_URL = `https://covers.openlibrary.org/b/isbn/${value}-L.jpg`
@@ -30,14 +30,21 @@ app.get("/new",(req,res)=>{
 })
 
 app.post("/add",async (req,res)=>{
-    const name = req.body.title
-    const creator = req.body.author
-    const crux = req.body.gist
-    const rate = req.body.rating
-    const code = req.body.isbn
-    await db.query("insert into booklist (title,author,gist,rating,isbn) values ($1,$2,$3,$4,$5)",[name,creator,crux,rate,code])
-    res.redirect("/")
+    try{
+        const name = req.body.title
+        const creator = req.body.author
+        const crux = req.body.gist
+        const rate = req.body.rating
+        const code = req.body.isbn
+        await db.query("insert into booklist (title,author,gist,rating,isbn) values ($1,$2,$3,$4,$5)",[name,creator,crux,rate,code])
+        res.redirect("/")
+    }catch(err){
+        console.log("Empty Fields")
+        res.redirect("/")
+    }
 })
+
+// this route is for the read more button, if we click on read more it will use this route
 app.post("/details", async (req,res)=>{
     const  id = req.body.bookId
     const result = await db.query("select * from booklist where id = ($1)",[id])
@@ -45,9 +52,22 @@ app.post("/details", async (req,res)=>{
     let value = books[0].isbn
     const API_URL = `https://covers.openlibrary.org/b/isbn/${value}-L.jpg`
     // const response = await axios.get(API_URL)
-    // console.log(response.data)
+    console.log(req.params.bookId)
     res.render("details.ejs",{items:books,cover:API_URL})
 })
+
+// this route is actually an alternative to the above route, here if a user click on the Image, it will work
+app.get("/detail/:id", async (req,res)=>{
+    const id = req.params.id
+    const result = await db.query("select * from booklist where id = ($1)",[id])
+    const books = result.rows
+    let value = books[0].isbn
+    const API_URL = `https://covers.openlibrary.org/b/isbn/${value}-L.jpg`
+    res.render("details.ejs",{items:books,cover:API_URL})
+})
+
+
+
 app.post("/update", async (req,res)=>{
     const name = req.body.title
     const creator = req.body.author
